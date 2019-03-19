c2d448b6cbae738017dc6e6bb6bd84d26325b45c
zhuofanw <zhuofanw@sfu.ca>
2019-03-19 00:58:58 -0700

    Prompt Component test

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,29 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
+import { Prompt } from 'react-router-dom';
+class About extends React.Component {
+    state = {
+        name: ""
+    }
 
-const About = (props) => {
-    return(
-        <div>
-            About page
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                About Page
+                <Prompt
+                    when={ !!this.state.name }
+                    message = "Do you want to leave?"
+                />
+                <div>
+                    <input
+                        value = {this.state.name}
+                        type="text"
+                        onChange={ e => this.setState({name: e.target.value})}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
+
 
 export default About;
