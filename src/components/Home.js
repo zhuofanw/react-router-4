7f454d521364d68b019a9bd5e840ee7c1c2d99a4
zhuofanw <zhuofanw@sfu.ca>
2019-03-19 00:44:53 -0700

    using withRouter HOC to wrap child component that  get 'props' data from App.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,12 @@
 import React, { PropTypes } from 'react';
+import {withRouter} from 'react-router';
+const Hello = (props) => {
+    console.log(props);
+    return (
+        <div><button onClick={ () => props.history.push('/about') }>Hello</button></div>
+    );
+}
+const WithRouterHello = withRouter(Hello);
 
 const Home = (props) => {
     console.log(props);
@@ -6,6 +14,7 @@ const Home = (props) => {
         <div>
             <button onClick={() => props.history.push('/about')}>click me</button>
             Welcome to Home
+            <WithRouterHello />
         </div>
     )
 }
